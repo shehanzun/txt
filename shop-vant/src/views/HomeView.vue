24d7455534981784077dfb88836f020120b8b5d5
shehanzun <2933882507@qq.com>
2024-06-16 16:46:54 +0800

    性能

---
+++ shop-vant/src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+    <img v-for="item in 10" alt="Vue logo" src="../assets/logo.png">
+    <button @click="gotop">top</button>
+  </div>
+</template>
+
+<style>
+img {
+  display: block;
+  height: 300px;
+  filter: drop-shadow(0px 0px 5px yellow);
+}
+
+button {
+  background-color: blue;
+  padding: 20px 30px;
+  border-radius: 15px;
+  color: white;
+  border: none;
+}
+</style>
+
+<script>
+ export default{
+  el: 'home',
+  data() {
+    return {
+      val:false
+    }
+  },
+
+  methods: {
+    gotop() {
+      this.val=true
+      if (this.val) {
+        
+        window.scrollTo({
+          top:899,
+          behavior:'smooth'
+        })
+        this.val=false
+      }
+    },
+  },
+}
+
+</script>
